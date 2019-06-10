83f36dd9af22e7b59e3884eb37ebe9217b7de932
Nicena Fontaine <nicenas.email@gmail.com>
2019-06-10 16:57:36 -0400

    Clean up comments and unused console.logs.
    Also change all language from error to alert, as both terms
    were being used interchangeably, which could be problematic
    in a larger app.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// grab the form
+// Global variables
+
 const form = document.getElementById('loan-form');
 const card = document.querySelector('.card');
 const loadingGif = document.getElementById('loading');
@@ -10,27 +11,23 @@ form.addEventListener('submit', revealResults);
 
 // event handler
 function revealResults(event) {
-  // console.log(`and the results are`); // verify it's linked
   hideElement(results);
-  displayElement(loadingGif); // nested display element and hide loadingGif in the if statement that determines there's an error to prevent the loadingGif from showing if the error is triggered
+  displayElement(loadingGif);
   calculateResults();
   event.preventDefault();
 }
 
+// hide/display elements (reusable functions)
 function displayElement(e) {
   e.style.display = 'block';
-  console.log(`showing ${e}`);
 }
 
 function hideElement(e) {
   e.style.display = 'none';
-  console.log(`hiding ${e}`);
 }
 
-// Math
+// Math it out
 function calculateResults() {
-  // Verify function is linked
-  // console.log('calculating')
   // Variables for calculations
   const loanAmt = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -38,15 +35,11 @@ function calculateResults() {
   const monthlyPmt = document.getElementById('monthly-payment');
   const ttlPmt = document.getElementById('total-payment');
   const ttlInterest = document.getElementById('total-interest');
-  // verify variables are linked to DOM
-  // console.log(`${loanAmt.value}, ${interest.value}, ${pmtYears.value}, ${
-  //    monthlyPmt.value}, ${ttlPmt.value}, ${ttlInterest.value}`);
 
   // Calculations
-  const principal = parseFloat(loanAmt.value); // turns it into a decimal
+  const principal = parseFloat(loanAmt.value);
   const calcInterest = parseFloat(interest.value) / 100 / 12;
   const calcPmts = parseFloat(pmtYears.value) * 12;
-
   const x = Math.pow(1 + calcInterest, calcPmts);
   const monthly = (principal * x * calcInterest) / (x - 1);
 
@@ -59,41 +52,28 @@ function calculateResults() {
       displayElement(results);
     }, 2000);
   } else {
-    // console.log(`this isn't finite`); // verify else works; code the showError function later
     hideElement(loadingGif);
-    showError();
+    showAlert();
   }
-
-  // event.preventDefault(); // Move up to the new revealResults event handler, as calculateResults() is now just a function and no longer an event handler.
 }
 
-// Show error
-
-function showError() {
+// Show alert
+function showAlert() {
   if (alreadyAlerted === false) {
     popupAlert('Please enter all of the values below.');
     alreadyAlerted = true;
-    setTimeout(clearError, 3000);
-  } else {
-    console.log(`there is already an alert`);
+    setTimeout(clearAlert, 3000);
   }
 }
 
-function popupAlert(errorMssg) {
-  const errorDiv = document.createElement('div');
-  errorDiv.className = 'alert alert-danger'; // bootstrap classes
-  errorDiv.appendChild(document.createTextNode(errorMssg));
-  // Insert error after heading
-  card.insertBefore(errorDiv, form);
+function popupAlert(alertMssg) {
+  const alertDiv = document.createElement('div');
+  alertDiv.className = 'alert alert-danger';
+  alertDiv.appendChild(document.createTextNode(alertMssg));
+  card.insertBefore(alertDiv, form);
 }
 
-function clearError() {
+function clearAlert() {
   document.querySelector('.alert').remove();
   alreadyAlerted = false;
 }
-
-/*
- * debug - can get multiple error messages in the 3 seconds
- *   1. disable the button right below error = true && enable it in clearError
- *   2. set a condition that must be met, like if alert === true, do not showError with the inital value of let alert = true; and then put alert = false to the clearError. ()
- */
